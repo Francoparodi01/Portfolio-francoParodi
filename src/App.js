6ca22ba81b19a39092f233899e46bb7b3f1208e8
unknown <francoparodi2001@gmail.com>
2023-08-24 19:27:41 -0300

    24/8

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Header from './components/Header';
 import Introduction from './components/Introduction';
 import Aboutme from './components/Aboutme'
 import Portfolio from './components/Portfolio';
+import VisibleComponents from './components/VisibleComponents';
 
 function App() {
   return (
     <>
+  <VisibleComponents>
     <Header/>
     <Introduction/>
     <Aboutme/>
     <Portfolio/>
+    </VisibleComponents>
     </>
   );
 }
