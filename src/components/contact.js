39f27c3379304a2c4bcf7cf34913609772c4f169
Francoparodi01 <francoparodi2001@gmail.com>
2023-10-18 13:37:33 -0300

    contacto portfolio

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {SiGmail} from "react-icons/si"
+import {FcContacts} from "react-icons/fc"
 
 const contact = () => {
   return (
-    <div>Si estas interesado en contactarme</div>
+    <section className='aboutme-section' id='contacto'>
+    <div className='section-cont'>
+    <h2 className='section-title'>Contacto <FcContacts className='mb-2'/></h2>
+    </div>
+    <ul className='menu-ul'>
+        <li>
+            <SiGmail color='red' className='ml-3'/> 
+            <a
+             href="https://mail.google.com/mail/u/1/#inbox?compose=GTvVlcSDXXrmlzJTTmGmpSTzBRmDxJLngdmsDQTLNWLWzwHmPCZsLmxJqPqSTvNHxBqfSVbwFqNnM"
+             className='repositorio-proyectos'
+             >
+                Francoparodi2001@gmail.com
+            </a>
+        </li>
+    </ul>
+    </section>
   )
 }
 
