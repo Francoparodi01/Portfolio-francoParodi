cfc6a84b861a6b715645da6929ae88fecdaea9bc
Francoparodi01 <francoparodi2001@gmail.com>
2023-11-14 19:54:20 -0300

    agregando gym app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Introduction from './components/Introduction';
 import Aboutme from './components/Aboutme'
 import Portfolio from './components/Portfolio';
 import VisibleComponents from './components/VisibleComponents';
-import Contact from './components/Contact';
+
 
 function App() {
   return (
@@ -15,7 +15,6 @@ function App() {
     <Introduction/>
     <Aboutme/>
     <Portfolio/>
-    <Contact/>
     </VisibleComponents>
     </>
   );
